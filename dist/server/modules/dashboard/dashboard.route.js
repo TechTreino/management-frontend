b9ce309509c64140c53cac7824d89e1cbc8fc408
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-02-12 00:06:59 -0200

    (1) Removido umas marotagens

--- dist/server/modules/dashboard/dashboard.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const dashboard_controller_1 = require("./dashboard.controller");
-class DashboardRoute {
-    static create(router) {
-        let controller = dashboard_controller_1.DashboardController.getInstance();
-        router.get("/dashboard", controller.data);
-    }
-}
-exports.DashboardRoute = DashboardRoute;
