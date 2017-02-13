028ea9d740e586293403b7929ac3b3e3c44846bd
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-02-13 00:08:28 -0200

    (1) Removendo algumas coisas

--- src/client/pages/customers/customers.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-	selector: "ad-page-customers"
-})
-export class CustomersComponent {}
--- src/client/pages/trainings/trainings.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-	selector: "ad-page-trainings"
-})
-export class TrainingsComponent {}
