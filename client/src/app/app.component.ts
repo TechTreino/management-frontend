fdba85fbbb862b7da77bd8a33bbfc7c8a9788e2c
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-02-16 22:27:15 -0200

    (1) Commitando antes de reorganizar os arquivos de build

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: "app-root",
-	template: "<router-outlet></router-outlet>"
+	template: "<router-outlet></router-outlet>",
+	styles: [ require("./app.component.sass").toString() ]
 })
 export class AppComponent {}
