a54d22b1bc797157ede255b1290504b95021b65e
wagoid <w.rsantos@outlook.com>
2017-06-27 01:38:50 -0300

    Remove headers hack

--- src/globals/js/services/auth-interceptor.factory.js
+++ src/globals/js/services/auth-interceptor.factory.js
@@ -7,16 +7,6 @@
 
   function factory(AcadAuth) {
     const authInfoKeys = ["access-token", "client", "expiry", "token-type", "uid"];
-    const authInfoPascalCaseMapping = {
-      "access-token": "Access-Token",
-      "client": "Client",
-      "expiry": "Expiry",
-      "token-type": "Token-Type",
-      "uid": "Uid",
-    };
-    const getHeader = function(headers, headerName) {
-      return headers[headerName] || headers[authInfoPascalCaseMapping[headerName]];
-    };
 
     return {
       request: function(config) {
@@ -39,14 +29,14 @@
       response: function(response) {
         const headers = response.headers();
         const hasAuthInfo = authInfoKeys.every(function(key) {
-          return Boolean(getHeader(headers, key));
+          return Boolean(headers[key]);
         });
 
         if (hasAuthInfo) {
           const newAuthInfo = {};
 
           authInfoKeys.forEach(function(key) {
-            newAuthInfo[key] = getHeader(headers, key);
+            newAuthInfo[key] = headers[key];
           });
           AcadAuth.setAuthInfo(newAuthInfo);
         }
