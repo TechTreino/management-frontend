54598536800972fb089a8d84cec54d0d5d3708fe
wagoid <w.rsantos@outlook.com>
2017-06-28 19:39:21 -0300

    Add request/transformers to transform camelCase keys to snake_case and vice versa

--- src/app.module.js
+++ src/app.module.js
@@ -1,63 +1,86 @@
 "use strict";
 
-angular
-	.module("AcademiaDigital", [
-		"ngRoute",
-		"AcadSidebar",
-		"AcadTopbar",
-		"AcadContent",
-		"AcadTitlebar",
-		"AcadTable",
-		"AcadBoxItem",
-		"AcadInputText",
-		"AcadInputPassword",
-		"AcadSelect",
-		"AcadForm",
-		"AcadModal",
-		"Auth",
-		"Customers",
-		"Exercises",
-		"MuscleGroups",
-		"Trainings"
-	])
-	.config(function($routeProvider, $locationProvider, $httpProvider, AcadSidebarProvider) {
-		$locationProvider.html5Mode(true);
-		const pagesPath = "./dist/templates/pages";
-		// Routes configuration
-		$routeProvider
-			.when("/", { redirectTo: "/customers" })
-			.when("/login", { templateUrl: `${pagesPath}/auth/login/template/login.template.html` })
-			.when("/customers", { templateUrl: `${pagesPath}/customers/template/customers.template.html` })
-			.when("/customers/create", { templateUrl: `${pagesPath}/customers/template/create-customer.template.html` })
-			.when("/customers/:id", { templateUrl: `${pagesPath}/customers/template/edit-customer.template.html` })
-			.when("/exercises", { templateUrl: `${pagesPath}/exercises/template/exercises.template.html` })
-			.when("/exercises/create", { templateUrl: `${pagesPath}/exercises/template/create-exercise.template.html` })
-			.when("/exercises/:id", { templateUrl: `${pagesPath}/exercises/template/edit-exercise.template.html` })
-			.when("/trainings", {
-				templateUrl: `${pagesPath}/trainings/template/trainings-customers-list.template.html`
-			})
-			.when("/trainings/:id", {
-				templateUrl: `${pagesPath}/trainings/template/trainings-customer.template.html`
+(function() {
+	angular
+		.module("AcademiaDigital", [
+			"ngRoute",
+			"AcadSidebar",
+			"AcadTopbar",
+			"AcadContent",
+			"AcadTitlebar",
+			"AcadTable",
+			"AcadBoxItem",
+			"AcadInputText",
+			"AcadInputPassword",
+			"AcadSelect",
+			"AcadForm",
+			"AcadModal",
+			"Auth",
+			"Customers",
+			"Exercises",
+			"MuscleGroups",
+			"Trainings"
+		])
+		.config(function($routeProvider, $locationProvider, $httpProvider, AcadSidebarProvider) {
+			$locationProvider.html5Mode(true);
+			const pagesPath = "./dist/templates/pages";
+			// Routes configuration
+			$routeProvider
+				.when("/", { redirectTo: "/customers" })
+				.when("/login", { templateUrl: `${pagesPath}/auth/login/template/login.template.html` })
+				.when("/customers", { templateUrl: `${pagesPath}/customers/template/customers.template.html` })
+				.when("/customers/create", {
+					templateUrl: `${pagesPath}/customers/template/create-customer.template.html`
+				})
+				.when("/customers/:id", { templateUrl: `${pagesPath}/customers/template/edit-customer.template.html` })
+				.when("/exercises", { templateUrl: `${pagesPath}/exercises/template/exercises.template.html` })
+				.when("/exercises/create", {
+					templateUrl: `${pagesPath}/exercises/template/create-exercise.template.html`
+				})
+				.when("/exercises/:id", { templateUrl: `${pagesPath}/exercises/template/edit-exercise.template.html` })
+				.when("/trainings", {
+					templateUrl: `${pagesPath}/trainings/template/trainings-customers-list.template.html`
+				})
+				.when("/trainings/:id", {
+					templateUrl: `${pagesPath}/trainings/template/trainings-customer.template.html`
+				});
+
+			// Menu configuration
+			AcadSidebarProvider.config.menu = [
+				{ id: "customers", text: "Alunos", link: "/customers/" },
+				{ id: "exercises", text: "Exercícios", link: "/exercises/" },
+				{ id: "trainings", text: "Fichas", link: "/trainings/" }
+			];
+
+			$httpProvider.interceptors.push("AcadAuthInterceptor");
+			$httpProvider.defaults.transformRequest.unshift(transformRequest);
+			$httpProvider.defaults.transformResponse.push(transformResponse);
+		})
+		.run(["$rootScope", "$location", "AcadAuth", function($rootScope, $location, AcadAuth) {
+			AcadAuth.initializeUserDataFromLocalstorage();
+
+			$rootScope.$on("$routeChangeStart", function(event) {
+				const isWhitelisted = $location.path() === "/login" || $location.path() === "/forgot";
+
+				if (!AcadAuth.isLoggedIn() && !isWhitelisted) {
+					event.preventDefault();
+					$location.path("/login");
+				}
 			});
+		}]);
 
-		// Menu configuration
-		AcadSidebarProvider.config.menu = [
-			{ id: "customers", text: "Alunos", link: "/customers/" },
-			{ id: "exercises", text: "Exercícios", link: "/exercises/" },
-			{ id: "trainings", text: "Fichas", link: "/trainings/" }
-		];
+	function transformRequest(data) {
+		if (angular.isObject(data)) {
+			return humps.decamelizeKeys(data);
+		}
 
-		$httpProvider.interceptors.push("AcadAuthInterceptor");
-	})
-	.run(["$rootScope", "$location", "AcadAuth", function($rootScope, $location, AcadAuth) {
-		AcadAuth.initializeUserDataFromLocalstorage();
+		return data;
+	}
+	function transformResponse(data) {
+		if (angular.isObject(data)) {
+			return humps.camelizeKeys(data);
+		}
 
-		$rootScope.$on("$routeChangeStart", function(event) {
-			const isWhitelisted = $location.path() === "/login" || $location.path() === "/forgot";
-
-			if (!AcadAuth.isLoggedIn() && !isWhitelisted) {
-				event.preventDefault();
-				$location.path("/login");
-			}
-		});
-	}]);
+		return data;
+	}
+})();
